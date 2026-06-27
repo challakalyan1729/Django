fc573c4a1234cc20a0c043ae28416091a0867cad
challakalyan1729 <challakalyan1729@gmail.com>
2026-06-27 16:57:37 +0530

    hello name

--- helloworld/homeapp/views.py
+++ helloworld/homeapp/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def fun(resquest):
-    return HttpResponse("home page")
-def fun2(resquest):
-    return HttpResponse("page1")
-def fun3(resquest):
-    return HttpResponse("page2")
+def fun(request):
+    response = render(request,"")
